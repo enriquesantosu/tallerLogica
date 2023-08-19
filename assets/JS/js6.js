35d2ddf79a5356f930d04fc3d71a7cb7b5b0f4a2
enriquesantosu <enriquesantosu@gmail.com>
2023-08-18 19:29:03 -0500

    acabado

--- assets/JS/js6.js
+++ assets/JS/js6.js
@@ -1,55 +1,52 @@
-const form = document.querySelector("#form_ejercicio6")
-const mensaje = document.querySelector("#valorMatricula")
+const form = document.querySelector("#form_ejercicio6");
+const mensaje = document.querySelector("#valorMatricula");
+let inputs = document.querySelectorAll(".inputMateria");
+let notas = document.querySelectorAll(".valorNota");
 
-const matricula = 1000000
+const matricula = 1000000;
+
+console.log(notas);
+
+notas.forEach((inputDeNotas) => {
+    inputDeNotas.addEventListener('input', (typeEvent) => {
+
+        console.log(typeEvent.target.value)
+
+
+    });
+});
+
+
+
 
 form.addEventListener('submit', (event6) => {
     event6.preventDefault();
-    const input6 = document.querySelector("#input_numero").value;
-    const parsedN6 = parseInt(input6);
 
-    console.log(parsedN6)
+    console.log('form submitted')
 
-    if (mostrarError5(input6, parsedN6)) {
+    let totalNotas = 0
 
-        console.log('numero valido')
+    notas.forEach((input) => {
+        console.log(totalNotas += parseFloat(input.value))
+    })
+
+    console.log(promedio = totalNotas / notas.length)
 
 
-        if (parsedN6 < 3) {
-            mensaje.innerHTML = 'No hay descuento y el valor de la matricula es de 1.000.000'
-        } else if (parsedN6 > 3 && parsedN6 <= 4) {
-            mensaje.innerHTML = `
-            Tiene un descuento del 5%. El valor de la matricula es de ${(matricula * 0.95)}`
-        } else {
-            mensaje.innerHTML = `
-            Tiene un descuento del 50%. El valor de la matricula es de ${(matricula * 0.50)}`
-        }
-
+    if (promedio < 3) {
+        mensaje.innerHTML = 'No hay descuento y el valor de la matricula es de 1.000.000'
+    } else if (promedio > 3 && promedio <= 4) {
+        mensaje.innerHTML = `
+                Tiene un descuento del 5%. El valor de la matricula es de ${(matricula * 0.95)}`
     } else {
-        console.log(-1);
+        mensaje.innerHTML = `
+                Tiene un descuento del 50%. El valor de la matricula es de ${(matricula * 0.50)}`
     }
 
 
+});
 
 
-})
 
-function mostrarError5(input6, parsedN6) {
 
-    if (input6 === "") {
-        errorNada = document.querySelector("#errorAlertEmpty");
-        errorNada.classList.remove('d-none');
-        console.log('error nada')
-        return false
-    } else if (parsedN6 < 0) {
-        errorCantidad = document.querySelector("#errorAlertAmount");
-        errorCantidad.classList.remove('d-none');
-        console.log('error cantidad');
-        return false
-    } else if (isNaN(parsedN6)) {
-        errorTipo = document.querySelector("#errorAlertType");
-        errorTipo.classList.remove('d-none');
-        console.log('error tipo');
-        return false
-    } else return true
-}
+
